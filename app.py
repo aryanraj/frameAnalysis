517bf7364d353a80cc89d9aff1027a806ec6dd0a
Vishrut Kumar Mishra <m.vishrut180795@gmail.com>
2015-05-03 04:44:28 +0530

    Added Tm_matrix created

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from numpy.linalg import inv
+from numpy import transpose
 import sys
 import math
 
@@ -9,15 +10,24 @@ class node:
 
 class member:
   def __init__(self, _node1, type1, _node2, type2, EI, EA):
-    self.L = math.sqrt(math.pow(_node1.x-_node2.x,2),math.pow(_node1.y-_node2.y,2))
-    self.S = (_node2.y - _node1.y)/self.L
-    self.C = (_node2.x - _node1.x)/self.L
     self.node1 = _node1
     self.type1 = type1
     self.node2 = _node2
     self.type2 = type2
     self.EI = EI
     self.EA = EA
+    self.build_T_matrix()
+
+  def build_T_matrix(self):
+    L = math.sqrt(math.pow(_node1.x-_node2.x,2),math.pow(_node1.y-_node2.y,2))
+    S = (_node2.y - _node1.y)/L
+    C = (_node2.x - _node1.x)/L
+    self.Tm = [
+      [ C,-S, 0],
+      [ S, C, 0],
+      [ 0, 0, 1]
+    ]
+    self.TmT = transpose(self.Tm)
 
 
 if __name__ == "__main__":
@@ -25,7 +35,8 @@ if __name__ == "__main__":
   print data
 
 
-""" Structure of data file
+""" 
+    Structure of data file
     node name x y
     member node1 type node2 type EI EA
 """

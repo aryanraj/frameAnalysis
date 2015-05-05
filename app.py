e6156641807a3977e9923c84b282d1fbe86e2734
Aryan Raj <aryan.raj15604@gmail.com>
2015-05-05 09:09:16 +0530

    Almost finished everything

--- app.py
+++ app.py
@@ -62,6 +62,12 @@ class member:
     self.type1 = _data[2]
     self.node2 = get_node_by_name(_data[3])
     self.type2 = _data[4]
+    self.FEM1 = 0
+    self.FEM2 = 0
+    self.FX1 = 0
+    self.FX2 = 0
+    self.FY1 = 0
+    self.FY2 = 0
     self.EI = float(_data[5])
     self.EA = float(_data[6])
     self.build_T_matrix()
@@ -164,21 +170,120 @@ class member:
     # print('\n'.join([''.join(['{:20}'.format(item) for item in row]) for row in K]))
     # pp(K)
 
-  def extract_R(self, R):
-    pass
+  def manage_forceX(self, data):
+    a = float(data[1])
+    b = self.L - a
+    P = float(data[2])
+    FX1 = -P*b/L
+    FX2 = -P*a/L
+    self.FX1 += FX1
+    self.FX2 += FX2
 
-def shuffle_matrix(K,r,R):
+  def force_node_type_M(self, M1, M2):
+    if self.type1 == "pinned" and self.type2 == "fixed":
+      M2 -= M1/2
+      M1 = 0
+    if self.type1 == "fixed" and self.type2 == "pinned":
+      M1 -= M2/2
+      M2 = 0
+    if self.type1 == "pinned" and self.type2 == "pinned":
+      M1 = 0
+      M2 = 0
+    return (M1,M2)
+
+  def manage_forceY(self, data):
+    a = float(data[1])
+    b = self.L - a
+    P = float(data[2])
+    L = self.L
+    M1 = -P*b**2*a/L**2
+    M2 = P*a**2*b/L**2
+    M1,M2 = self.force_node_type_M(M1,M2)
+    FY1 = -P*b/L + (M1+M2)/L
+    FY2 = -P*a/L - (M1+M2)/L
+    self.FEM1 += M1
+    self.FEM2 += M2
+    self.FY1 += FY1
+    self.FY2 += FY2
+
+  def manage_LDL(self, data):
+    pos = data[1]
+    W = float(data[2])
+    L = self.L
+    if pos == "left":
+      M1 = -W*L**2/20
+      M2 = W*L**2/30
+      M1,M2 = self.force_node_type_M(M1,M2)
+      FY1 = -W*L/3 + (M1+M2)/L
+      FY2 = -W*L/6 - (M1+M2)/L
+    elif pos == "right":
+      M1 = -W*L**2/30
+      M2 = W*L**2/20
+      M1,M2 = self.force_node_type_M(M1,M2)
+      FY1 = -W*L/6 + (M1+M2)/L
+      FY2 = -W*L/3 - (M1+M2)/L
+    self.FEM1 += M1
+    self.FEM2 += M2
+    self.FY1 += FY1
+    self.FY2 += FY2
+
+  def manage_UDL(self, data):
+    W = float(data[1])
+    L = self.L
+    M1 = -W*L**2/12
+    M2 = W*L**2/12
+    M1,M2 = self.force_node_type_M(M1,M2)
+    FY1 = -W*L/2
+    FY2 = -W*L/2
+    self.FEM1 += M1
+    self.FEM2 += M2
+    self.FY1 += FY1
+    self.FY2 += FY2    
+
+  def manage_moment(self, data):
+    a = float(data[1])
+    b = self.L - a
+    M = float(data[2])
+    L = self.L
+    M1 = -M*b/L**2*(2*a-b)
+    M2 = -M*a/L**2*(2*b-a)
+    M1,M2 = self.force_node_type_M(M1,M2)
+    FY1 = (M1+M2)/L
+    FY2 = -(M1+M2)/L
+    self.FEM1 += M1
+    self.FEM2 += M2
+    self.FY1 += FY1
+    self.FY2 += FY2
+
+  def extract_C(self, C):
+    FL = [ self.FX1 , self.FY1 , self.FEM1 , self.FX2 , self.FY2 , self.FEM2 ]
+    FG = np.dot(self.L2G,FL)
+    s = (int(self.node1.name)-1)*3
+    t = (int(self.node2.name)-1)*3
+    
+    C[s+0] += FG[0]
+    C[s+1] += FG[1]
+    C[s+2] += FG[2]
+    C[t+0] += FG[3]
+    C[t+1] += FG[4]
+    C[t+2] += FG[5]
+
+
+def shuffle_matrix(K,r,R,C):
   temp_K = []
   temp_R = []
+  temp_C = []
   count = 0
   for i in range(len(R)):
     if isinstance(R[i], basestring):
       temp_K.insert(0,K[i])
       temp_R.insert(0,R[i])
+      temp_C.insert(0,C[i])
       count += 1
     else:
       temp_K.append(K[i])
       temp_R.append(R[i])
+      temp_C.append(C[i])
 
   K = transpose(temp_K)
   temp_K = []
@@ -199,10 +304,12 @@ def shuffle_matrix(K,r,R):
   D = np.array(temp_K)[count:len(R),count:len(R)]
   E = np.array(temp_r)[0:count]
   X = np.array(temp_r)[count:len(R)]
-  Y = np.array(temp_R)[0:count]
   F = np.array(temp_R)[count:len(R)]
+  Y = np.array(temp_R)[0:count]
+  G = np.array(temp_C)[0:count]
+  H = np.array(temp_C)[count:len(R)]
 
-  return (A,B,C,D,E,F,X,Y)
+  return (A,B,C,D,E,F,G,H,X,Y)
 
 
 if __name__ == "__main__":
@@ -213,20 +320,31 @@ if __name__ == "__main__":
         nodes.append(node(_data))
       elif _data[0] == "member":
         members.append(member(_data))
+      elif _data[0] == "forceX":
+        members[len(members)-1].manage_forceX(_data)
+      elif _data[0] == "forceY":
+        members[len(members)-1].manage_forceY(_data)
+      elif _data[0] == "UDL":
+        members[len(members)-1].manage_UDL(_data)
+      elif _data[0] == "LDL":
+        members[len(members)-1].manage_LDL(_data)
+      elif _data[0] == "moment":
+        members[len(members)-1].manage_moment(_data)
   
   len_K = len(nodes)
   K_matrix = [[ 0 for j in range(len_K*3) ] for i in range(len_K*3)]
   R_matrix = [0 for i in range(len_K*3)]
   r_matrix = [0 for i in range(len_K*3)]
+  C_matrix = [0 for i in range(len_K*3)]
 
   for _member in members:
     _member.extract_K(K_matrix)
-    _member.extract_R(R_matrix)
+    _member.extract_C(C_matrix)
   for _node in nodes:
     _node.extract_R(R_matrix)
     _node.extract_r(r_matrix)
 
-  A,B,C,D,E,F,X,Y = shuffle_matrix(K_matrix,r_matrix,R_matrix)
+  A,B,C,D,E,F,G,H,X,Y = shuffle_matrix(K_matrix,r_matrix,R_matrix,C_matrix)
 
   A = np.array(A,dtype=float)
   B = np.array(B,dtype=float)
@@ -234,9 +352,11 @@ if __name__ == "__main__":
   D = np.array(D,dtype=float)
   E = np.array(E,dtype=float)
   F = np.array(F,dtype=float)
+  G = np.array(G,dtype=float)
+  H = np.array(H,dtype=float)
 
-  _X = np.dot(inv(D),np.subtract(F,np.dot(C,E)))
-  _Y = np.add(np.dot(B,_X),np.dot(A,E))
+  _X = np.dot(inv(D),np.subtract(np.subtract(F,np.dot(C,E)),H))
+  _Y = np.add(np.add(np.dot(B,_X),np.dot(A,E)),G)
 
   print "A"
   print('\n'.join([''.join(['{:15}'.format(item) for item in row]) for row in A]))
@@ -259,20 +379,20 @@ if __name__ == "__main__":
   print X,_X
   print Y,_Y
 
-  # pp(X)
-  # pp(_X)
-  # pp(Y)
-  # pp(_Y)
-
 
 """
     node name x y fx fy fr
     member node1 type node2 type EI EA
+    forceX x` magnitude
+    forceY x` magnitude
+    UDL magnitude
+    LDL pos magnitude
+    moment x` magnitude 
 """
 
 """
-    [ A B ][E] = [Y]
-    [ C D ][X] = [F]
-    X = inv(D)*(F-C*E)
-    Y = B*X+A*E
+    [ A B ][E] = [Y]-[G]
+    [ C D ][X] = [F]-[H]
+    X = inv(D)*(F-C*E-H)
+    Y = B*X+A*E+G
 """
